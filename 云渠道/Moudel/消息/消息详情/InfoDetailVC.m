9d7cc06cad97328d1ccbbcd8ea1b95c2dbeb294b
wjq <378589569@qq.com>
2018-03-26 14:07:50 +0800

    1
    1

--- 云渠道/Moudel/消息/消息详情/InfoDetailVC.m
+++ 云渠道/Moudel/消息/消息详情/InfoDetailVC.m
@@ -8,30 +8,95 @@
 
 #import "InfoDetailVC.h"
 
-@interface InfoDetailVC ()
+@interface InfoDetailVC ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic , strong) UITableView *Maintableview;
+@property (nonatomic , strong) UIView *toolview;
 
+-(void)initUI;
+-(void)initDataSouce;
 @end
 
 @implementation InfoDetailVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.navBackgroundView.hidden = NO;
+    self.titleLabel.text = @"消息详情";
+    _Maintableview.rowHeight = 150 *SIZE;
+    _Maintableview.estimatedRowHeight = UITableViewAutomaticDimension;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)initUI
+{
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)initDataSouce
+{
+    
 }
-*/
+
+#pragma mark    -----  delegate   ------
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+    
+}
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    
+    static NSString *CellIdentifier = @"SystemMessageCell";
+    
+    SystemMessageCell *cell  = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[SystemMessageCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+    
+    
+    
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    InfoDetailVC * next_vc =[[InfoDetailVC alloc]init];
+    [self.navigationController pushViewController:next_vc animated:YES];
+}
+
+
+
+#pragma mark    -----  懒加载    ------
+
+-(UITableView *)Maintableview
+{
+    if (!_Maintableview) {
+        _Maintableview = [[UITableView alloc]initWithFrame:CGRectMake(0, NAVIGATION_BAR_HEIGHT, 360*SIZE, SCREEN_Height-NAVIGATION_BAR_HEIGHT-46.3*SIZE) style:UITableViewStylePlain];
+        _Maintableview.backgroundColor = YJBackColor;
+        _Maintableview.delegate = self;
+        _Maintableview.dataSource = self;
+        [_Maintableview setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    }
+    return _Maintableview;
+}
+
+
+-(UIView *)toolview
+{
+    if (!_toolview) {
+        _toolview  = [[UIView alloc]initWithFrame:CGRectMake(0, SCREEN_Height-TAB_BAR_HEIGHT, 360, TAB_BAR_HEIGHT)];
+    }
+    return _toolview;
+}
 
 @end
