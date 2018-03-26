04a4886284864f25416600c39009d06b194cf3b6
wjq <378589569@qq.com>
2018-03-26 13:28:16 +0800

    1
    1

---
+++ 云渠道/Moudel/消息/消息详情/InfoDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  InfoDetailVC.h
+//  云渠道
+//
+//  Created by xiaoq on 2018/3/26.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface InfoDetailVC : BaseViewController
+
+@end
