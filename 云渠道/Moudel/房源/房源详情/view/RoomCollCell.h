2f626c24b2742a1081bd8c1fcf5ed2bba2d4d0ee
xiaoginzq <122574357@QQ.COM>
2018-03-26 16:07:24 +0800

    添加Cell
    添加Cell

---
+++ 云渠道/Moudel/房源/房源详情/view/RoomCollCell.h
@@ -0,0 +1,13 @@
+//
+//  RoomCollCell.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/3/26.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoomCollCell : UICollectionViewCell
+
+@end
---
+++ 云渠道/Moudel/房源/房源详情/view/RoomCollCell.m
@@ -0,0 +1,13 @@
+//
+//  RoomCollCell.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/3/26.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "RoomCollCell.h"
+
+@implementation RoomCollCell
+
+@end
