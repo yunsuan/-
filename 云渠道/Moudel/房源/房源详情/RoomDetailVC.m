1667a8d358cc9f0c690dd4aee3a20e07d459cb41
xiaoginzq <122574357@QQ.COM>
2018-03-26 16:24:00 +0800

    修改问题
    修改问题

--- 云渠道/Moudel/房源/房源详情/RoomDetailVC.m
+++ 云渠道/Moudel/房源/房源详情/RoomDetailVC.m
@@ -7,9 +7,13 @@
 //
 
 #import "RoomDetailVC.h"
+//#import "RoomCollCell.h"
 
-@interface RoomDetailVC ()<UICollectionViewDelegate,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
-
+@interface RoomDetailVC ()<UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
+{
+    
+    NSArray *_titleArr;
+}
 @property (nonatomic, strong) UICollectionView *segmentColl;
 
 @property (nonatomic, strong) UICollectionViewFlowLayout *flowLayout;
@@ -21,7 +25,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self initDataSource];
     [self initUI];
+}
+
+- (void)initDataSource{
+    
+    _titleArr = @[@"详情",@"佣金",@"分析"];
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    
+    return 3;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    RoomCollCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"RoomCollCell" forIndexPath:indexPath];
+    if (!cell) {
+        
+        cell = [[RoomCollCell alloc] initWithFrame:CGRectMake(0, 0, 64 *SIZE, 44 *SIZE)];
+    }
+    cell.titleL.text = 
+    
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
 }
 
 - (void)initUI{
@@ -30,6 +62,13 @@
     self.titleLabel.hidden = NO;
     
     
+    
+    _segmentColl = [[UICollectionView alloc] initWithFrame:CGRectMake(84 *SIZE, 20 *SIZE, 192 *SIZE, 44 *SIZE) collectionViewLayout:_flowLayout];
+    _segmentColl.backgroundColor = CH_COLOR_white;
+    _segmentColl.delegate = self;
+    _segmentColl.dataSource = self;
+    [_segmentColl registerClass:[RoomCollCell class] forCellWithReuseIdentifier:@"RoomCollCell"];
+    [self.navBackgroundView addSubview:_segmentColl];
 }
 
 
