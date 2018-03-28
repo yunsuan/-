993c1dd106f71d9c7eb19840053f1ac13979ab2e
wjq <378589569@qq.com>
2018-03-28 14:22:57 +0800

    1
    1

---
+++ 云渠道/Moudel/工作/推荐/cell/RecommendCell.h
@@ -0,0 +1,13 @@
+//
+//  RecommendCell.h
+//  云渠道
+//
+//  Created by xiaoq on 2018/3/28.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecommendCell : UITableViewCell
+
+@end
---
+++ 云渠道/Moudel/工作/被推荐的客户/cell/NomineeCell.m
@@ -0,0 +1,24 @@
+//
+//  NomineeCell.m
+//  云渠道
+//
+//  Created by xiaoq on 2018/3/28.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "NomineeCell.h"
+
+@implementation NomineeCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
