16a7695a048ca22f577e7df7231f537392627dc9
xiaoginzq <122574357@QQ.COM>
2018-03-28 14:44:03 +0800

    添加公司Model
    添加公司Model

---
+++ 云渠道/Moudel/我的/工作经历/选择公司/model/CompanyModel.h
@@ -0,0 +1,13 @@
+//
+//  CompanyModel.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/3/28.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CompanyModel : NSObject
+
+@end
