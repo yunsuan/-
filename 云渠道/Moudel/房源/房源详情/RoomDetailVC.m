32576dad01e5b50cb3093461fae21e361846f85e
wjq <378589569@qq.com>
2018-03-28 09:29:55 +0800

    1
    1

--- 云渠道/Moudel/房源/房源详情/RoomDetailVC.m
+++ 云渠道/Moudel/房源/房源详情/RoomDetailVC.m
@@ -50,23 +50,23 @@
     
     _titleArr = @[@"详情",@"佣金",@"分析"];
     
-    //初始化实例
-    _locationManager = [[BMKLocationService alloc] init];
-    //设置delegate
-    _locationManager.delegate = self;
-    //设置返回位置的坐标系类型
-//    _locationManager.coordinateType = BMKLocationCoordinateTypeBMK09LL;
-    //设置距离过滤参数
-    _locationManager.distanceFilter = kCLDistanceFilterNone;
-    //设置预期精度参数
-    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    //设置应用位置类型
-//    _locationManager.activityType = CLActivityTypeAutomotiveNavigation;
-    //设置是否自动停止位置更新
-    _locationManager.pausesLocationUpdatesAutomatically = NO;
-    //设置是否允许后台定位
-    _locationManager.allowsBackgroundLocationUpdates = YES;
-    //设置位置获取超时时间
+//    //初始化实例
+//    _locationManager = [[BMKLocationService alloc] init];
+//    //设置delegate
+//    _locationManager.delegate = self;
+//    //设置返回位置的坐标系类型
+////    _locationManager.coordinateType = BMKLocationCoordinateTypeBMK09LL;
+//    //设置距离过滤参数
+//    _locationManager.distanceFilter = kCLDistanceFilterNone;
+//    //设置预期精度参数
+//    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+//    //设置应用位置类型
+////    _locationManager.activityType = CLActivityTypeAutomotiveNavigation;
+//    //设置是否自动停止位置更新
+//    _locationManager.pausesLocationUpdatesAutomatically = NO;
+//    //设置是否允许后台定位
+//    _locationManager.allowsBackgroundLocationUpdates = YES;
+//    //设置位置获取超时时间
 }
 
 - (void)ActionMoreBtn:(UIButton *)btn{
