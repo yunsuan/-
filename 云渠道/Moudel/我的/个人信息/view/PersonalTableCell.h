21c17ca2c29ee307be096a23165cb485527c6da0
xiaoginzq <122574357@QQ.COM>
2018-03-28 17:56:22 +0800

    添加view文件夹
    添加view文件夹

---
+++ 云渠道/Moudel/我的/个人信息/view/PersonalTableCell.h
@@ -0,0 +1,13 @@
+//
+//  PersonalTableCell.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/3/28.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonalTableCell : UITableViewCell
+
+@end
---
+++ 云渠道/Moudel/我的/个人信息/view/PersonalTableCell.m
@@ -0,0 +1,24 @@
+//
+//  PersonalTableCell.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/3/28.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "PersonalTableCell.h"
+
+@implementation PersonalTableCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
