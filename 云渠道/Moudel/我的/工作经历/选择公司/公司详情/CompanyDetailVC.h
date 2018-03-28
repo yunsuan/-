6c069c3c4f6f2f52a8fa6e5b1832dd90a64d18b5
xiaoginzq <122574357@QQ.COM>
2018-03-28 14:16:39 +0800

    添加公司详情文件夹
    添加公司详情文件夹

---
+++ 云渠道/Moudel/我的/工作经历/选择公司/公司详情/CompanyDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  CompanyDetailVC.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/3/28.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface CompanyDetailVC : BaseViewController
+
+@end
