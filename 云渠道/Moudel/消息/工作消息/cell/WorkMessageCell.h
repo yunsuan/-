888248e571e41354750a6793f7fa6afb170ecd8a
wjq <378589569@qq.com>
2018-03-28 11:21:06 +0800

    1
    1

---
+++ 云渠道/Moudel/消息/工作消息/cell/WorkMessageCell.h
@@ -0,0 +1,13 @@
+//
+//  WorkMessageCell.h
+//  云渠道
+//
+//  Created by xiaoq on 2018/3/28.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorkMessageCell : UITableViewCell
+
+@end
---
+++ 云渠道/Moudel/消息/工作消息/cell/WorkMessageCell.m
@@ -0,0 +1,24 @@
+//
+//  WorkMessageCell.m
+//  云渠道
+//
+//  Created by xiaoq on 2018/3/28.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "WorkMessageCell.h"
+
+@implementation WorkMessageCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
