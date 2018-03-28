8c51eb1967e2e9529d1c3bf8a75f3433c94b0b87
wjq <378589569@qq.com>
2018-03-28 14:17:29 +0800

    1
    1

--- 云渠道/Moudel/工作/推荐/RecommendVC.m
+++ 云渠道/Moudel/工作/推荐/RecommendVC.m
@@ -8,30 +8,136 @@
 
 #import "RecommendVC.h"
 
-@interface RecommendVC ()
+@interface RecommendVC ()<UITableViewDelegate,UITableViewDataSource>
 
+@property (nonatomic , strong) UITableView *MainTableView;
+
+-(void)initUI;
+-(void)initDateSouce;
 @end
 
 @implementation RecommendVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = YJBackColor;
+    self.navBackgroundView.hidden = NO;
+    self.leftButton.hidden = YES;
+    self.titleLabel.text = @"推荐";
+    self.leftButton.hidden = YES;
+    [self initDateSouce];
+    [self initUI];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)initDateSouce
+{
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)initUI
+{
+    
+    [self.view addSubview:self.MainTableView];
 }
-*/
+
+
+
+
+#pragma mark  ---  delegate   ---
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 2;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+    
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 190*SIZE;
+}
+
+//-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    static NSString *CellIdentifier = @"WorkingCell";
+//
+//    WorkingCell *cell  = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+//    if (!cell) {
+//        cell = [[WorkingCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+//    }
+//    [cell setTitle:_namelist[indexPath.row] content:@"今日新增 2，有效 21，修改 39" img:_imglist[indexPath.row]];
+//    return cell;
+//
+//}
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //    switch (indexPath.row) {
+    //        case 0:
+    //        {
+    //            MyCommissionVC *next_vc = [[MyCommissionVC alloc]init];
+    //            [self.navigationController pushViewController:next_vc animated:YES];
+    //        }
+    //            break;
+    //        case 1:
+    //        {
+    //            MyAttentionVC *next_vc = [[MyAttentionVC alloc]init];
+    //            [self.navigationController pushViewController:next_vc animated:YES];
+    //        }
+    //            break;
+    //        case 2:
+    //        {
+    //            FixPassWrodVC *next_vc = [[FixPassWrodVC alloc]init];
+    //            [self.navigationController pushViewController:next_vc animated:YES];
+    //
+    //        }
+    //            break;
+    //        case 3:
+    //        {
+    //            AbortVC *next_vc = [[AbortVC alloc]init];
+    //            [self.navigationController pushViewController:next_vc animated:YES];
+    //        }
+    //            break;
+    //        case 4:
+    //        {
+    //    [self alertControllerWithNsstring:@"温馨提示" And:@"你确定要退出当前账号吗？" WithCancelBlack:^{
+    //
+    //    } WithDefaultBlack:^{
+    //        [[NSUserDefaults standardUserDefaults] removeObjectForKey:LOGINENTIFIER];
+    //        [[NSNotificationCenter defaultCenter]postNotificationName:@"goLoginVC" object:nil];
+    //
+    //    }];
+    
+    //        }
+    //            break;
+    //
+    //        default:
+    //            break;
+    //    }
+}
+
+
+
+#pragma mark  ---  懒加载   ---
+-(UITableView *)MainTableView
+{
+    if(!_MainTableView)
+    {
+        _MainTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, NAVIGATION_BAR_HEIGHT+1*SIZE, 360*SIZE, SCREEN_Height-NAVIGATION_BAR_HEIGHT-1*SIZE) style:UITableViewStylePlain];
+        _MainTableView.backgroundColor = YJBackColor;
+        _MainTableView.delegate = self;
+        _MainTableView.dataSource = self;
+        [_MainTableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+        //        _MainTableView.scrollEnabled = NO;
+    }
+    return _MainTableView;
+}
+
+
 
 @end
