db719c9ab4ae041612258ab40c8b23d55e68d80c
xiaoginzq <122574357@QQ.COM>
2018-03-29 10:14:01 +0800

    添加佣金Cell
    添加佣金Cell

---
+++ 云渠道/Moudel/我的/我的佣金/view/BrokerageTableCell.m
@@ -0,0 +1,24 @@
+//
+//  BrokerageTableCell.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/3/29.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BrokerageTableCell.h"
+
+@implementation BrokerageTableCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
