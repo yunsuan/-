f03bf1681a716e6d249d4cae43a3f7c3cefb2232
xiaoginzq <122574357@QQ.COM>
2018-03-29 17:40:48 +0800

    添加关注cell
    添加关注cell

--- 云渠道/Moudel/我的/我的关注/MyAttentionVC.m
+++ 云渠道/Moudel/我的/我的关注/MyAttentionVC.m
@@ -16,22 +16,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self initUI];
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)initUI{
+    
+    self.navBackgroundView.hidden = NO;
+    self.titleLabel.text = @"我的关注";
+    
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
