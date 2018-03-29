df71dc331742984e9717e8824ef332c2b00c7194
xiaoginzq <122574357@QQ.COM>
2018-03-29 13:38:58 +0800

    已结列表添加文件夹
    已结列表添加文件夹

---
+++ 云渠道/Moudel/我的/我的佣金/已结佣金/view/BrokerageCell.h
@@ -0,0 +1,13 @@
+//
+//  BrokerageCell.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/3/29.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrokerageCell : UITableViewCell
+
+@end
