f9378c87c80250a128ff96b202aa701e0a294475
wjq <378589569@qq.com>
2018-03-29 18:08:19 +0800

    1
    1

---
+++ 云渠道/自定义SDK/自定义TextField/BorderTF.h
@@ -0,0 +1,13 @@
+//
+//  BorderTF.h
+//  云渠道
+//
+//  Created by xiaoq on 2018/3/29.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BorderTF : UIView
+@property (nonatomic , strong) UITextField *textfield;
+@end
