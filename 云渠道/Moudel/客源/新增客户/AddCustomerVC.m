87a22c3a88fd64d1f0f70d948d7de82e053acaaf
wjq <378589569@qq.com>
2018-03-29 13:18:11 +0800

    1
    1

--- 云渠道/Moudel/客源/新增客户/AddCustomerVC.m
+++ 云渠道/Moudel/客源/新增客户/AddCustomerVC.m
@@ -7,9 +7,13 @@
 //
 
 #import "AddCustomerVC.h"
+#import "SinglePickView.h"
 
 @interface AddCustomerVC ()
-//@property (nonatomic , strong)  *<#object#>;
+@property (nonatomic , strong) UIScrollView *scrollview;
+
+-(void)initUI;
+-(void)initDataSouce;
 @end
 
 @implementation AddCustomerVC
@@ -19,11 +23,24 @@
     // Do any additional setup after loading the view.
     self.navBackgroundView.hidden = NO;
     self.titleLabel.text = @"添加客户";
+    [self initUI];
+    [self initDataSouce];
 }
 
+-(void)initUI
+{
+//    SinglePickView *view = [[SinglePickView alloc]initWithFrame:self.view.frame WithData:@[@{@"MC":@"男",
+//                                                                                             @"ID":@"1"
+//                                                                                             },@{@"MC":@"女",
+//                                                                                                 @"ID":@"2"
+//                                                                                                 }]];
+//    [self.view addSubview:view];
+}
 
-
-
+-(void)initDataSouce
+{
+    
+}
 
 
 @end
