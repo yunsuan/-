e76f7fbe75d1833c64c17fff66b7a536ece3604e
xiaoginzq <122574357@QQ.COM>
2018-03-30 09:48:44 +0800

    添加关注view文件夹
    添加关注view文件夹

---
+++ 云渠道/Moudel/我的/我的关注/view/AttentionTableCell.h
@@ -0,0 +1,13 @@
+//
+//  AttentionTableCell.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/3/30.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttentionTableCell : UITableViewCell
+
+@end
