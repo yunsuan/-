cbfe7b9b560ab8feb6833769217c71d1ba42c475
xiaoginzq <122574357@QQ.COM>
2018-03-30 15:39:11 +0800

    文件夹布局
    文件夹布局

---
+++ 云渠道/Moudel/我的/公司认证/AuthenticationVC.m
@@ -0,0 +1,300 @@
+//
+//  AuthenticationVC.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/3/23.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "AuthenticationVC.h"
+#import "AuthenTableCell.h"
+#import "AuthenCollCell.h"
+#import "SelectCompanyVC.h"
+
+@interface AuthenticationVC ()<UITableViewDelegate,UITableViewDataSource,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout,UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+{
+    
+    NSArray *_titleArr;
+    NSMutableArray *_contentArr;
+    NSMutableArray *_imgArr;
+    UIImagePickerController *_imagePickerController;
+    UIImage *_image;
+    NSInteger _index;
+}
+@property (nonatomic, strong) UITableView *authenTable;
+
+@property (nonatomic, strong) UICollectionView *authenColl;
+
+@property (nonatomic, strong) UICollectionViewFlowLayout *flowLayout;
+
+@property (nonatomic, strong) UIScrollView *scrollView;
+
+@property (nonatomic, strong) UIButton *commitBtn;
+
+@end
+
+@implementation AuthenticationVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+ 
+    [self initDataSource];
+    [self initUI];
+}
+
+- (void)initDataSource{
+    
+    _contentArr = [[NSMutableArray alloc] init];
+    _imgArr = [[NSMutableArray alloc] init];
+    _titleArr = @[@"所属公司",@"工号",@"角色",@"申请项目",@"所属部门",@"职位",@"入职/申请时间"];
+    _imagePickerController = [[UIImagePickerController alloc] init];
+    _imagePickerController.delegate = self;
+}
+
+- (void)ActionCancelBtn:(UIButton *)btn{
+    [_imgArr removeObjectAtIndex:btn.tag];
+    [self.authenColl reloadData];
+}
+
+#pragma mark --table代理
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 7;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 51 *SIZE;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    NSString * Identifier = @"AuthenTableCell";
+    AuthenTableCell *cell = [tableView dequeueReusableCellWithIdentifier:Identifier];
+    if (!cell) {
+        
+        cell = [[AuthenTableCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:Identifier];
+    }
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    cell.titleL.text = _titleArr[indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    if (!indexPath.row) {
+        
+        SelectCompanyVC *nextVC = [[SelectCompanyVC alloc] init];
+        [self.navigationController pushViewController:nextVC animated:YES];
+    }
+}
+
+
+#pragma mark --coll代理
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    
+    return _imgArr.count < 3? _imgArr.count + 1: 3;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    AuthenCollCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"AuthenCollCell" forIndexPath:indexPath];
+    if (!cell) {
+        
+        cell = [[AuthenCollCell alloc] initWithFrame:CGRectMake(0, 0, 120 *SIZE, 91 *SIZE)];
+    }
+    cell.cancelBtn.tag = indexPath.item;
+    cell.cancelBtn.hidden = NO;
+    [cell.cancelBtn addTarget:self action:@selector(ActionCancelBtn:) forControlEvents:UIControlEventTouchUpInside];
+    if (_imgArr.count) {
+        
+        if (indexPath.item < _imgArr.count) {
+            
+            cell.imageView.image = _imgArr[indexPath.item];
+        }else{
+            
+            cell.imageView.image = [UIImage imageNamed:@"add"];
+            cell.cancelBtn.hidden = YES;
+        }
+    }else{
+        
+        cell.imageView.image = [UIImage imageNamed:@"add"];
+        cell.cancelBtn.hidden = YES;
+    }
+    
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    _index = indexPath.item;
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"选择照片" message:@"" preferredStyle:UIAlertControllerStyleActionSheet];
+    UIAlertAction *photo = [UIAlertAction actionWithTitle:@"相册" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        [self selectPhotoAlbumPhotos];
+    }];
+    UIAlertAction *takePic = [UIAlertAction actionWithTitle:@"拍照" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        [self takingPictures];
+    }];
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    [alertController addAction:takePic];
+    [alertController addAction:photo];
+    [alertController addAction:cancel];
+    [self.navigationController presentViewController:alertController animated:YES completion:^{
+        
+    }];
+}
+
+#pragma mark - 选择头像
+
+- (void)selectPhotoAlbumPhotos {
+    // 获取支持的媒体格式
+    NSArray *mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    // 判断是否支持需要设置的sourceType
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+        
+        // 1、设置图片拾取器上的sourceType
+        _imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        // 2、设置支持的媒体格式
+        _imagePickerController.mediaTypes = @[mediaTypes[0]];
+        // 3、其他设置
+        _imagePickerController.allowsEditing = YES; // 如果设置为NO，当用户选择了图片之后不会进入图像编辑界面。
+        // 4、推送图片拾取器控制器
+        [self presentViewController:_imagePickerController animated:YES completion:nil];
+    }
+}
+
+// 拍照
+- (void)takingPictures {
+    // 获取支持的媒体格式
+    NSArray *mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
+    
+    // 判断是否支持需要设置的sourceType
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        
+        // 1、设置图片拾取器上的sourceType
+        _imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+        
+        // 2、设置支持的媒体格式
+        _imagePickerController.mediaTypes = @[mediaTypes[0]];
+        // 3、其他设置
+        // 设置相机模式
+        _imagePickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
+        // 设置摄像头：前置/后置
+        _imagePickerController.cameraDevice = UIImagePickerControllerCameraDeviceRear;
+        // 设置闪光模式
+        _imagePickerController.cameraFlashMode = UIImagePickerControllerCameraFlashModeAuto;
+        
+        // 4、推送图片拾取器控制器
+        [self presentViewController:_imagePickerController animated:YES completion:nil];
+        
+    } else {
+        NSLog(@"当前设备不支持拍照");
+        UIAlertController * alertController = [UIAlertController alertControllerWithTitle:@"温馨提示"
+                                                                                  message:@"当前设备不支持拍照"
+                                                                           preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:[UIAlertAction actionWithTitle:@"确定"
+                                                            style:UIAlertActionStyleDefault
+                                                          handler:^(UIAlertAction *action) {
+                                                              //                                                              _uploadButton.hidden = NO;
+                                                          }]];
+        [self presentViewController:alertController
+                           animated:YES
+                         completion:nil];
+    }
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    if (picker.sourceType == UIImagePickerControllerSourceTypeCamera) {
+        if ([info[UIImagePickerControllerMediaType] isEqualToString:@"public.image"]) {
+            
+            _image = info[UIImagePickerControllerOriginalImage];;
+            
+            if (_index < _imgArr.count - 1) {
+                
+                [_imgArr replaceObjectAtIndex:_index withObject:_image];
+            }else{
+                
+                [_imgArr addObject:_image];
+            }
+        }
+    }else if (picker.sourceType == UIImagePickerControllerSourceTypePhotoLibrary){
+        
+        _image = info[UIImagePickerControllerOriginalImage];
+        
+        if (_index < _imgArr.count) {
+            
+            [_imgArr replaceObjectAtIndex:_index withObject:_image];
+        }else{
+            
+            [_imgArr addObject:_image];
+        }
+    }
+    [self dismissViewControllerAnimated:YES completion:^{
+        
+        [self.authenColl reloadData];
+        
+    }];
+}
+
+// 用户点击了取消按钮
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [self.presentedViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)initUI{
+    
+    self.titleLabel.text = @"认证信息填写";
+    self.navBackgroundView.hidden = NO;
+    
+    _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, NAVIGATION_BAR_HEIGHT, SCREEN_Width, SCREEN_Height - NAVIGATION_BAR_HEIGHT)];
+    _scrollView.backgroundColor = self.view.backgroundColor;
+    _scrollView.contentSize = CGSizeMake(SCREEN_Width, 672 *SIZE);
+    _scrollView.bounces = NO;
+    [self.view addSubview:_scrollView];
+    
+    _authenTable = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_Width, 361 *SIZE) style:UITableViewStylePlain];
+    _authenTable.backgroundColor = self.view.backgroundColor;
+    _authenTable.delegate = self;
+    _authenTable.dataSource = self;
+    _authenTable.bounces = NO;
+    _authenTable.separatorStyle = UITableViewCellSeparatorStyleNone;
+//    _authenTable.tableFooterView = [[UIView alloc] init];
+    [_scrollView addSubview:_authenTable];
+    
+    UIView *whiteView = [[UIView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(_authenTable.frame), SCREEN_Width, 174 *SIZE)];
+    whiteView.backgroundColor = CH_COLOR_white;
+    [_scrollView addSubview:whiteView];
+    
+    _flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    _flowLayout.itemSize = CGSizeMake(120 *SIZE, 91 *SIZE);
+    _flowLayout.minimumLineSpacing = 0;
+    _flowLayout.minimumInteritemSpacing = 0;
+    
+    _authenColl = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 50 *SIZE, SCREEN_Width, 91 *SIZE) collectionViewLayout:_flowLayout];
+    _authenColl.backgroundColor = CH_COLOR_white;
+    _authenColl.delegate = self;
+    _authenColl.dataSource = self;
+    
+    [_authenColl registerClass:[AuthenCollCell class] forCellWithReuseIdentifier:@"AuthenCollCell"];
+    [whiteView addSubview:_authenColl];
+    
+    _commitBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    _commitBtn.frame = CGRectMake(21 *SIZE, 37 *SIZE + CGRectGetMaxY(whiteView.frame), 317 *SIZE, 40 *SIZE);
+    _commitBtn.layer.masksToBounds = YES;
+    _commitBtn.layer.cornerRadius = 2 *SIZE;
+    _commitBtn.backgroundColor = YJLoginBtnColor;
+    [_commitBtn setTitle:@"提交申请" forState:UIControlStateNormal];
+    [_commitBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    _commitBtn.titleLabel.font = [UIFont systemFontOfSize:16 *SIZE];
+//    [_commitBtn addTarget:self action:@selector(Login) forControlEvents:UIControlEventTouchUpInside];
+    [_scrollView addSubview:_commitBtn];
+}
+
+@end
