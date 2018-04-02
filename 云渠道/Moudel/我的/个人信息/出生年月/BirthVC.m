191c2ecde93e5a91cdcd67da116ec3edc09e757c
xiaoginzq <122574357@QQ.COM>
2018-04-02 13:10:11 +0800

    添加出生年月
    添加出生年月

---
+++ 云渠道/Moudel/我的/个人信息/出生年月/BirthVC.m
@@ -0,0 +1,37 @@
+//
+//  BirthVC.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/4/2.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BirthVC.h"
+
+@interface BirthVC ()
+
+@end
+
+@implementation BirthVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
