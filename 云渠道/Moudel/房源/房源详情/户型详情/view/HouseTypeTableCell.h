986d9ed83dd32140aa618cf120078874f0b14867
xiaoginzq <122574357@QQ.COM>
2018-04-04 13:58:52 +0800

    添加cell
    添加cell

---
+++ 云渠道/Moudel/房源/房源详情/户型详情/view/HouseTypeTableCell.h
@@ -0,0 +1,13 @@
+//
+//  HouseTypeTableCell.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/4/4.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface HouseTypeTableCell : BaseViewController
+
+@end
