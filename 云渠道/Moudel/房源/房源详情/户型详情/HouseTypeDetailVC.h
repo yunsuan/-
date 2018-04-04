698a83152257649f2e3bd9ad58d9d0cf3236afcf
xiaoginzq <122574357@QQ.COM>
2018-04-04 13:20:47 +0800

    添加户型详情
    添加户型详情

---
+++ 云渠道/Moudel/房源/房源详情/户型详情/HouseTypeDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  HouseTypeDetailVC.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/4/4.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface HouseTypeDetailVC : BaseViewController
+
+@end
