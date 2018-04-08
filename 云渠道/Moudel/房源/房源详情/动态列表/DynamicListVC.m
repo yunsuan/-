289932a0792ef68a322fe4dd15ddcba0cd95babf
xiaoginzq <122574357@QQ.COM>
2018-04-08 16:32:35 +0800

    添加动态列表文件夹
    添加动态列表文件夹

---
+++ 云渠道/Moudel/房源/房源详情/动态列表/DynamicListVC.m
@@ -0,0 +1,31 @@
+//
+//  DynamicListVC.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/4/8.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "DynamicListVC.h"
+
+@interface DynamicListVC ()
+
+
+
+@end
+
+@implementation DynamicListVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+    [self initUI];
+}
+
+- (void)initUI{
+    
+    self.navBackgroundView.hidden = NO;
+    
+}
+
+@end
