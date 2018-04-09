a68b4fbe9e89f629119cfa8dea54668e27dfe977
xiaoginzq <122574357@QQ.COM>
2018-04-09 11:02:24 +0800

    添加文件夹
    添加文件夹

---
+++ 云渠道/Moudel/工作/推荐/待确认详情/UnconfirmDetailVC.m
@@ -0,0 +1,37 @@
+//
+//  UnconfirmDetailVC.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/4/9.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "UnconfirmDetailVC.h"
+
+@interface UnconfirmDetailVC ()
+
+@end
+
+@implementation UnconfirmDetailVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
