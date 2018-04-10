97bb7125c21ec400a0ae24bcc154cb3e137582c0
xiaoginzq <122574357@QQ.COM>
2018-04-10 10:30:40 +0800

    推荐弹出框
    推荐弹出框

---
+++ 云渠道/自定义SDK/弹出框/RecommendView.h
@@ -0,0 +1,33 @@
+//
+//  RecommendView.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/4/10.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecommendView : UIView
+
+@property (nonatomic, strong) UIView *whiteView;
+
+@property (nonatomic, strong) UILabel *codeL;
+
+@property (nonatomic, strong) UILabel *nameL;
+
+@property (nonatomic, strong) UILabel *projectL;
+
+@property (nonatomic, strong) UILabel *addressL;
+
+@property (nonatomic, strong) UILabel *contactL;
+
+@property (nonatomic, strong) UILabel *phoneL;
+
+@property (nonatomic, strong) UILabel *timeL;
+
+@property (nonatomic, strong) UIButton *tranmitBtn;
+
+@property (nonatomic, strong) UIButton *confirmBtn;
+
+@end
