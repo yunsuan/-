f403f356f401fdd982479842166d046d80337e9b
xiaoginzq <122574357@QQ.COM>
2018-04-11 15:07:17 +0800

    添加基类model
    添加基类model

---
+++ 云渠道/BaseClass/BaseModel.h
@@ -0,0 +1,16 @@
+//
+//  BaseModel.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/4/11.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseModel : NSObject
+
+- (NSMutableDictionary *)modeltodic;
+- (instancetype)initWithDictionary:(NSDictionary *)dict;
+
+@end
