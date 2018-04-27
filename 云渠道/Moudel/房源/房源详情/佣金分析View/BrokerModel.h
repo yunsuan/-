f3d70e8a1607da3468058642820331ea88407d45
wjq <378589569@qq.com>
2018-04-27 13:35:27 +0800

    1
    1

---
+++ 云渠道/Moudel/房源/房源详情/佣金分析View/BrokerModel.h
@@ -0,0 +1,14 @@
+//
+//  BrokerModel.h
+//  云渠道
+//
+//  Created by xiaoq on 2018/4/27.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BrokerModel : NSObject
+
+
+@end
---
+++ 云渠道/Moudel/房源/房源详情/佣金分析View/BrokerModel.m
@@ -0,0 +1,13 @@
+//
+//  BrokerModel.m
+//  云渠道
+//
+//  Created by xiaoq on 2018/4/27.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BrokerModel.h"
+
+@implementation BrokerModel
+
+@end
