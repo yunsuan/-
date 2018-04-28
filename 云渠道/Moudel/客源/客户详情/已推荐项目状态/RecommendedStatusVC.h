a8fb662481799d9e84753f2cff4b24ebc849343b
xiaoginzq <122574357@QQ.COM>
2018-04-28 17:14:51 +0800

    添加已推荐项目状态文件夹
    添加已推荐项目状态文件夹

---
+++ 云渠道/Moudel/客源/客户详情/已推荐项目状态/RecommendedStatusVC.h
@@ -0,0 +1,13 @@
+//
+//  RecommendedStatusVC.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/4/28.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface RecommendedStatusVC : BaseViewController
+
+@end
