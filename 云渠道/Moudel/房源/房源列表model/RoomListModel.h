db69581714adfb81c38a719f83a75c9a350dda48
xiaoginzq <122574357@QQ.COM>
2018-05-03 10:31:34 +0800

    房源排名
    房源排名

--- 云渠道/Moudel/房源/房源列表model/RoomListModel.h
+++ 云渠道/Moudel/房源/房源列表model/RoomListModel.h
@@ -11,16 +11,21 @@
 @interface RoomListModel : NSObject
 
 @property (nonatomic, strong) NSString *absolute_address;
-@property (nonatomic , strong) NSString *city;
-@property (nonatomic , strong) NSString *deposit;
-@property (nonatomic , strong) NSString *district;
-@property (nonatomic , strong) NSString *img_url;
+@property (nonatomic, strong) NSString *brokerSortCompare;
+@property (nonatomic, strong) NSString *city;
+@property (nonatomic, strong) NSString *cycle;
+@property (nonatomic, strong) NSString *deposit;
+@property (nonatomic, strong) NSString *district;
+@property (nonatomic, strong) NSString *img_url;
 @property (nonatomic, strong) NSString *project_id;
-@property (nonatomic , strong) NSString *project_name;
-@property (nonatomic , strong) NSString *project_tags;
+@property (nonatomic, strong) NSString *project_name;
+@property (nonatomic, strong) NSString *project_tags;
+@property (nonatomic, strong) NSMutableArray *property_tags;
 @property (nonatomic, strong) NSString *province;
 @property (nonatomic, strong) NSString *sale_state;
-@property (nonatomic, strong) NSMutableArray *property_tags;
+@property (nonatomic, strong) NSString *sort;
+
+
 
 - (NSMutableDictionary *)modeltodic;
 - (instancetype)initWithDictionary:(NSDictionary *)dict;
