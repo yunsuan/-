84f764ced7aeb0ccb7d59268e20a4e435dcfb9c5
xiaoginzq <122574357@QQ.COM>
2018-05-05 15:22:59 +0800

    添加快速推荐房源给客户
    添加快速推荐房源给客户

---
+++ 云渠道/Moudel/客源/客户详情/快速推荐房源/QuickRoomVC.h
@@ -0,0 +1,17 @@
+//
+//  QuickRoomVC.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/5/5.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BaseViewController.h"
+#import "CustomRequireModel.h"
+
+@interface QuickRoomVC : BaseViewController
+
+- (instancetype)initWithModel:(CustomRequireModel *)model;
+
+@end
+
