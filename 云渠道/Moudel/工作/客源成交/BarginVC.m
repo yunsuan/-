5b0e9915df608207b7437abb53ea38d0c1d3efd7
xiaoginzq <122574357@QQ.COM>
2018-05-08 14:42:18 +0800

    添加成交客户
    添加成交客户

---
+++ 云渠道/Moudel/工作/客源成交/BarginVC.m
@@ -0,0 +1,37 @@
+//
+//  BarginVC.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/5/8.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BarginVC.h"
+
+@interface BarginVC ()
+
+@end
+
+@implementation BarginVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
