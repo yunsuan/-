2740a3e5d89328d9dd688ce7258108a2bbf0d535
wjq <378589569@qq.com>
2018-05-11 14:42:28 +0800

    3
    2

--- 云渠道/自定义SDK/加载动画/WaitAnimation.m
+++ 云渠道/自定义SDK/加载动画/WaitAnimation.m
@@ -24,7 +24,7 @@ static UIImageView *_imageV ;
         static dispatch_once_t onceToken;
         dispatch_once(&onceToken, ^{
             _imageV = [[UIImageView alloc]init];
-            _imageV.center = [UIApplication sharedApplication].keyWindow.center;
+            _imageV.center = CGPointMake(SCREEN_Width/2, SCREEN_Height/2);
             _imageV.bounds = CGRectMake(0, 0, 67, 43);
             NSMutableArray *imageArray = [NSMutableArray array];
             for (int i = 0; i < 7; i ++) {
@@ -34,7 +34,7 @@ static UIImageView *_imageV ;
             }
             _imageV.animationImages = imageArray;
             //2.2图片播放一次所需时长
-            _imageV.animationDuration = 1.0f;
+            _imageV.animationDuration = 0.5f;
             //2.3图片播放次数,0表示无限
             _imageV.animationRepeatCount = 0;
         });
