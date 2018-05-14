4538b9d89e564ddec56683328c3b36548bad6e3b
xiaoginzq <122574357@QQ.COM>
2018-05-14 17:03:25 +0800

    添加城市界面
    添加城市界面

---
+++ 云渠道/Moudel/房源/城市/CityVC.h
@@ -0,0 +1,13 @@
+//
+//  CityVC.h
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/5/14.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface CityVC : BaseViewController
+
+@end
---
+++ 云渠道/Moudel/房源/城市/CityVC.m
@@ -0,0 +1,37 @@
+//
+//  CityVC.m
+//  云渠道
+//
+//  Created by 谷治墙 on 2018/5/14.
+//  Copyright © 2018年 xiaoq. All rights reserved.
+//
+
+#import "CityVC.h"
+
+@interface CityVC ()
+
+@end
+
+@implementation CityVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
