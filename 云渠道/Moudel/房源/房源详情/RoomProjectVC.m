93724ec0c4a3141733de764775dad6079af66169
wjq <378589569@qq.com>
2018-05-15 20:58:03 +0800

    3
    3

--- 云渠道/Moudel/房源/房源详情/RoomProjectVC.m
+++ 云渠道/Moudel/房源/房源详情/RoomProjectVC.m
@@ -154,11 +154,8 @@
 }
 
 - (void)RequestMethod{
-    
-    
     NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
     [dic setObject:_projectId forKey:@"project_id"];
-    
      [dic setObject:[UserModelArchiver unarchive].agent_id forKey:@"agent_id"];
 //    [dic setObject:@"1" forKey:@"agent_id"];
     [BaseRequest GET:ProjectDetail_URL parameters:dic success:^(id resposeObject) {
