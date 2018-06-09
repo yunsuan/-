674dd5ac98507822d2f96d478fd566e9a7d96383
wjq <378589569@qq.com>
2018-06-09 17:23:56 +0800

    3
    3

--- 云渠道/Moudel/房源/房源详情/佣金分析View/BrokerModel.m
+++ 云渠道/Moudel/房源/房源详情/佣金分析View/BrokerModel.m
@@ -22,8 +22,15 @@
         
         
         for (int i = 0; i<data.count; i++) {
+            NSString *describe;
+            if ([data[i][@"desscribe"] isKindOfClass:[NSNull class]]) {
+                describe = @"";
+            }
+            else{
+                describe = data[i][@"describe"];
+            }
             NSDictionary *companydic = @{
-                                         @"basic":data[i][@"describe"],
+                                         @"basic":describe,
                                          @"begin_time":data[i][@"begin_time"],
                                          @"end_time":data[i][@"end_time"]
 //                                         @"basic" :[NSString stringWithFormat:@"1.推荐后，%@分钟内等信息被确认有效，收到推荐有效信息后%@日内结佣;\n\n2.开发商确认客户有效后%@日内结佣;\n\n3.客户%@日内签订合同，收到成交信息后%@日内结佣",data[i][@"visit_confirm_time"],data[i][@"confirm_settle_commission_time"],data[i][@"visit_settle_commission_time"],data[i][@"make_bargain_time"],data[i][@"region_settle_commission_time"]],
@@ -37,6 +44,7 @@
             NSArray *arr = data[i][@"person"];
             [_dataarr addObjectsFromArray:arr];
             for (int j = 0 ; j<arr.count; j++) {
+                
                 NSDictionary *dic = @{
 //                                      @"basic" :[NSString stringWithFormat:@"1.推荐后，%@分钟内等信息被确认有效，收到推荐有效信息后%@日内结佣;\n\n2.开发商确认客户有效后%@日内结佣;\n\n3.客户%@日内签订合同，收到成交信息后%@日内结佣",data[i][@"visit_confirm_time"],data[i][@"confirm_settle_commission_time"],data[i][@"visit_settle_commission_time"],data[i][@"make_bargain_time"],data[i][@"region_settle_commission_time"]]
                                       @"basic":data[i][@"describe"]
@@ -53,7 +61,12 @@
 {
     NSMutableArray *arr = [NSMutableArray array];
     for (int i = 0; i<_dataarr.count; i++) {
+        if ([_dataarr[i][@"commission_describe"] isKindOfClass:[NSNull class]]) {
+            [arr addObject:@""];
+        }
+        else{
         [arr addObject:_dataarr[i][@"commission_describe"]];
+        }
     }
     self.breakerinfo = arr;
 }
