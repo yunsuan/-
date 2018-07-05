5d3829c8bd2a7de512a9df6de55002de8efe3ad0
wjq <378589569@qq.com>
2018-07-05 09:37:51 +0800

    版本控制

--- 云渠道/UserModel/NetConfig.h
+++ 云渠道/UserModel/NetConfig.h
@@ -16,12 +16,17 @@
 //
 //static NSString *const Base_Net = @"http://120.27.21.136:2798/";
 
-//演示接口
 
-static NSString *const TestBase_Net = @"http://47.106.39.169:2797/";
+//可切换接口
 
-////基础接口正式
+//static NSString *const TestBase_Net =
+
+////演示接口
 //
+//static NSString *const TestBase_Net = @"http://47.106.39.169:2797/";
+
+//基础接口正式
+
 //static NSString *const TestBase_Net = @"http://120.78.69.178:2902/";
 
 
