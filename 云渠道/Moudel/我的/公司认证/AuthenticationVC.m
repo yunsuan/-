ed0cc9c1d23685f19fcb3f21289b6a031b4f5c4b
xiaoginzq <122574357@QQ.COM>
2018-07-08 15:06:28 +0800

    修改认证
    修改认证

--- 云渠道/Moudel/我的/公司认证/AuthenticationVC.m
+++ 云渠道/Moudel/我的/公司认证/AuthenticationVC.m
@@ -113,7 +113,7 @@
     
     if ([_roleL.text isEqualToString:@"到访确认人"]) {
         
-        if (!_projectId) {
+        if (!_projectId.length) {
             
             [self showContent:@"请选择申请项目"];
             return;
@@ -225,6 +225,8 @@
                 
                 _companyId = companyId;
                 _companyL.text = name;
+                _projectL.text = @"";
+                _projectId = @"";
             };
             [self.navigationController pushViewController:nextVC animated:YES];
             break;
@@ -243,6 +245,8 @@
                
                 _role = @"1";
                 _roleL.text = @"经纪人";
+                _projectL.text = @"";
+                _projectId = @"";
             }];
             
             UIAlertAction *comfirm = [UIAlertAction actionWithTitle:@"到访确认人" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
